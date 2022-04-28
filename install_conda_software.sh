70867a3793011bdbdf16e4c6a0cb158140215733
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-04-28 14:12:57 +0100

    Update install_conda_software.sh

--- install_conda_software.sh
+++ install_conda_software.sh
@@ -105,14 +105,14 @@ conda install -c conda-forge r-plyr=1.8.6 --yes
 conda install -c conda-forge r-dplyr=1.0.7 --yes
 conda deactivate
 
-//checkout the deago repos - fix
-conda create deago-1.1.3
+//checkout the deago repos
+conda create -n deago-1.1.3
 conda activate deago-1.1.3
 conda install -c bioconda perl-app-cpanminus=1.7043
 conda install -c bioconda perl-lwp-protocol-https=6.06 
 conda install -c bioconda perl-bioperl=1.6.924
 cpanm Dist::Zilla@5.048
-conda install bioconductor-deseq2=1.34.0 bioconductor-topgo=2.46.0 r-devtools=2.4.3 pandoc=2.18 bioconductor-limma=3.50.1 r-reshape=0.8.9 r-ggrepel=0.9.1 ggpubr=0.4.0 r-proto=1.0.0 r-markdown=1.1 r-rmarkdown=2.14
+conda install bioconductor-deseq2=1.34.0 bioconductor-topgo=2.46.0 r-devtools=2.4.3 pandoc=2.18 bioconductor-limma=3.50.1 r-reshape=0.8.9 r-ggrepel=0.9.1 r-ggpubr=0.4.0 r-proto=1.0.0 r-markdown=1.1 r-rmarkdown=2.14
 wget https://github.com/vaofford/deago/archive/refs/tags/v1.1.3.tar.gz
 wget https://github.com/vaofford/Bio-Deago/archive/refs/tags/v1.0.0.tar.gz
 tar -xvf v1.0.0.tar.gz
@@ -132,11 +132,13 @@ conda deactivate
 //Install qualifyr
 conda create -n qualifyr-1.4.6 python=3.8
 conda activate qualifyr-1.4.6 
-wget/git clone https://gitlab.com/cgps/qualifyr.git
-cd qualifyr
-git checkout 4d61a4d9
+wget https://gitlab.com/cgps/qualifyr/-/archive/1.4.6/qualifyr-1.4.6.tar.gz
+tar -xf qualifyr-1.4.6.tar.gz
+cd qualifyr-1.4.6/
 python setup.py install
 cd ..
+rm qualifyr-1.4.6.tar.gz
+rm -rf qualifyr-1.4.6
 conda deactivate
 
 //Install metagm

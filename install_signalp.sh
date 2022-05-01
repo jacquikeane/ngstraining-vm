06a539a0884d57882263c929b9c99a6a4a11f1ab
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-05-01 16:28:29 +0100

    Update install_signalp.sh

--- install_signalp.sh
+++ install_signalp.sh
@@ -2,6 +2,29 @@
 
 set -eu
 
-# Script assumes a user exists called software and that the script is run as user software
+# Script assumes a user exists called software and that the source for signalp exists and that the script is run as user software
 
 source $MINICONDA/etc/profile.d/conda.sh
+
+# Install signalp and add it to the prokka environment
+
+conda create -n signalp-6.0 python
+conda activate signalp-6.0 
+conda install tqdm>4.46.1 matplotlib>3.3.2 numpy>1.19.2
+pip install torch
+pip install signalp-6-package
+# copy the databases
+SIGNALP_DIR=$(python3 -c "import signalp; import os; print(os.path.dirname(signalp.__file__))" )
+cp -r signalp-6-package/models/* $SIGNALP_DIR/model_weights/
+conda deactivate
+
+conda activate prokka 
+conda install tqdm>4.46.1 matplotlib>3.3.2 numpy>1.19.2
+pip install torch
+pip install signalp-6-package/
+#copy the databases
+SIGNALP_DIR=$(python3 -c "import signalp; import os; print(os.path.dirname(signalp.__file__))" )
+cp -r signalp-6-package/models/* $SIGNALP_DIR/model_weights/
+conda deactivate
+
+set +eu

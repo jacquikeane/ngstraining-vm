72ea8e0e0cee387fe2517f91812327f95552c2d1
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-05-01 16:29:52 +0100

    Create install_qualifyr.sh

---
+++ install_qualifyr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+# Script assumes a user exists called software and that the source for signalp exists and that the script is run as user software
+
+source $MINICONDA/etc/profile.d/conda.sh
+
+# Install qualifyr
+conda create -n qualifyr-1.4.6 python=3.8
+conda activate qualifyr-1.4.6 
+wget https://gitlab.com/cgps/qualifyr/-/archive/1.4.6/qualifyr-1.4.6.tar.gz
+tar -xf qualifyr-1.4.6.tar.gz
+cd qualifyr-1.4.6/
+python setup.py install
+cd ..
+rm qualifyr-1.4.6.tar.gz
+rm -rf qualifyr-1.4.6
+conda deactivate
+
+set +eu

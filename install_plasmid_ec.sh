bafa68f1842f0157dc31a7cfbc113be986a39f9a
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-05-01 16:41:52 +0100

    Update install_plasmid_ec.sh

--- install_plasmid_ec.sh
+++ install_plasmid_ec.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+set -eu
+
+# Script assumes a user exists called software and that the script is run as user software
+
+source $MINICONDA/etc/profile.d/conda.sh
 
 # Get the source for plasmid EC and symlink to executables
 

56178edf82a35040572cdab75c53ac6c48c0141e
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-06-23 12:49:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,19 +9,15 @@ set -eu
 ./install_conda_software.sh
 ./check_conda_software.sh
 
-./install_conda_profiles.sh
-
 conda activate bactopia
 bactopia download
 
 ./install_bio_metagenomics.sh
-./install_deago.sh
-./install_plamid_ec.sh
 ./install_qualifyr.sh
 ./install_remove_blocks.sh
-./install_sh16_bactgen_scripts.sh
-./install_signalp.sh
 
 ./set_up_databases.sh
 
 ./install_nf_pipelines.sh
+
+./setup_references.sh
